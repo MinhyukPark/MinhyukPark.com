103b4d6621b5caf8147010ae6e306c38ba8e4e17
Minhyuk <minpark815@gmail.com>
2017-06-14 07:05:55 -0500

    Increase animated scroll duration

--- js/notmyscript.js
+++ js/notmyscript.js
@@ -1,20 +1,19 @@
 $(document).ready(function() {
-$('a[href*=#]').each(function() {
-if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-&amp;amp;&amp;amp; location.hostname == this.hostname
-&amp;amp;&amp;amp; this.hash.replace(/#/,'') ) {
-var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
-var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
-if ($target) {
-var targetOffset = $target.offset().top;
-$(this).click(function() {
-$("#nav li a").removeClass("active");
-$(this).addClass('active');
-$('html, body').animate({scrollTop: targetOffset}, 1000);
-return false;
-});
-}
-}
-});
-
+    $('a[href*=#]').each(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+            &amp;amp;&amp;amp; location.hostname == this.hostname
+            &amp;amp;&amp;amp; this.hash.replace(/#/,'') ) {
+            var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
+            var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
+            if ($target) {
+                var targetOffset = $target.offset().top;
+                $(this).click(function() {
+                    $("#nav li a").removeClass("active");
+                    $(this).addClass('active');
+                    $('html, body').animate({scrollTop: targetOffset}, 3000);
+                    return false;
+                });
+            }
+        }
+    });
 });
