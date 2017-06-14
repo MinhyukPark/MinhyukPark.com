f845661f17fa6e979d6a3d6fd196e84514e5ed40
Minhyuk <minpark815@gmail.com>
2017-06-14 08:37:55 -0500

    Replace scripts

--- js/notmyscript.js
+++ js/notmyscript.js
@@ -1,19 +1,24 @@
-$(document).ready(function() {
-    $('a[href*=#]').each(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-            &amp;amp;&amp;amp; location.hostname == this.hostname
-            &amp;amp;&amp;amp; this.hash.replace(/#/,'') ) {
-            var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
-            var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
-            if ($target) {
-                var targetOffset = $target.offset().top;
-                $(this).click(function() {
-                    $("#nav li a").removeClass("active");
-                    $(this).addClass('active');
-                    $('html, body').animate({scrollTop: targetOffset}, 200);
-                    return false;
-                });
-            }
-        }
-    });
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 });
