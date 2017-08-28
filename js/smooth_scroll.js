030f9632d95335a7f02f1016d52559f8dddc60b3
Minhyuk <minpark815@gmail.com>
2017-08-28 12:11:45 -0500

    Add description to illini laundry

--- js/smooth_scroll.js
+++ js/smooth_scroll.js
@@ -1,3 +1,4 @@
+/* https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_eff_animate_smoothscroll */
 $(document).ready(function(){
   // Add smooth scrolling to all links
   $("a").on('click', function(event) {
